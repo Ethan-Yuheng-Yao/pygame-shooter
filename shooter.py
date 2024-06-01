b85b7fd1fb4f19040a31e35a864ff5dbc73374d3
Ethan-Yuheng-Yao <ethanyhyao@gmail.com>
2024-06-01 17:34:04 +1000

    Added Score and Death Screen

--- shooter.py
+++ shooter.py
@@ -5,11 +5,14 @@ from sys import exit
 
 pygame.init()
 
+score = 0
 wave = 0
 enemies_to_spawn = 0
 enemies_spawned = 0
 time_since_last_spawn = 0
 spawn_timer = 0
+
+running = True
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -34,10 +37,14 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=pos)
 
     def update(self, bullets_group):
+        global score, running
         self.rect.x -= 1
         bullet_hits = pygame.sprite.spritecollide(self, bullets_group, True)
         if bullet_hits:
             self.kill()
+            score += 1
+        if self.rect.colliderect(end_point_rect):
+            running = False
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, pos, angle):
@@ -66,6 +73,11 @@ bullets_group = pygame.sprite.Group()
 enemies_group = pygame.sprite.Group()
 
 font = pygame.font.SysFont(None, 36)  
+font_ = pygame.font.SysFont(None, 60)
+font__ = pygame.font.SysFont(None, 100)
+
+end_text = font_.render('Game Over!', True, 'Red')
+end_text_rect = end_text.get_rect(center = (325, 100))
 
 spawn_interval = 1000  
 enemy_spawn_interval = 100  
@@ -75,7 +87,7 @@ clock = pygame.time.Clock()
 def start_new_wave():
     global wave, enemies_to_spawn, enemies_spawned, time_since_last_spawn
     wave += 1
-    enemies_to_spawn = random.randint(wave + 2, wave + 10)
+    enemies_to_spawn = random.randint(wave*2, wave*4)
     enemies_spawned = 0
     time_since_last_spawn = 0
 
@@ -86,50 +98,63 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                rel_x, rel_y = mouse_pos[0] - player.rect.centerx, mouse_pos[1] - player.rect.centery
-                angle = math.degrees(math.atan2(-rel_y, -rel_x))
-                angle += 180
-                bullet = Bullet(player.rect.center, angle)
-                bullets_group.add(bullet)
+        
+        if running:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mouse_pos = pygame.mouse.get_pos()
+                    rel_x, rel_y = mouse_pos[0] - player.rect.centerx, mouse_pos[1] - player.rect.centery
+                    angle = math.degrees(math.atan2(-rel_y, -rel_x))
+                    angle += 180
+                    bullet = Bullet(player.rect.center, angle)
+                    bullets_group.add(bullet)
+        else:
+            screen.fill((255, 255, 255))
+            screen.blit(end_text, end_text_rect)
+            score_text_end = font__.render(f'Final Score: {score}', True, (255, 0, 0))
+            score_text_end_rect = score_text_end.get_rect(center=(375, 300))
+            screen.blit(score_text_end, score_text_end_rect)
+            pygame.display.update()
+            continue
 
-    wave_text = font.render(f'Wave: {wave}', True, (255, 255, 255))
-    wave_text_rect = wave_text.get_rect(center=(325, 30))
+    if running:
+        wave_text = font.render(f'Wave: {wave}', True, (255, 255, 255))
+        wave_text_rect = wave_text.get_rect(center=(200, 30))
 
-    mouse_pos = pygame.mouse.get_pos()
-    player.update(mouse_pos)
+        score_text = font.render(f'Score: {score}', True, (255, 255, 255))
+        score_text_rect = score_text.get_rect(center=(500, 30))
 
-    bullets_group.update()
+        mouse_pos = pygame.mouse.get_pos()
+        player.update(mouse_pos)
 
-    screen.fill((0, 0, 0))
-    screen.blit(end_point, end_point_rect)
-    screen.blit(wave_text, wave_text_rect)
-    player_group.draw(screen)
+        bullets_group.update()
 
-    enemies_group.update(bullets_group)
-    enemies_group.draw(screen)
+        screen.fill((0, 0, 0))
+        screen.blit(end_point, end_point_rect)
+        screen.blit(wave_text, wave_text_rect)
+        screen.blit(score_text, score_text_rect)
+        player_group.draw(screen)
 
-    bullets_group.draw(screen)
+        enemies_group.update(bullets_group)
+        enemies_group.draw(screen)
 
-    pygame.display.update()
+        bullets_group.draw(screen)
+
+        pygame.display.update()
+
+        if enemies_spawned < enemies_to_spawn:
+            time_since_last_spawn += clock.get_time()
+            if time_since_last_spawn >= enemy_spawn_interval:
+                enemy_start_pos = (800, random.randint(15, 635))
+                enemy = Enemy(enemy_start_pos)
+                enemies_group.add(enemy)
+                time_since_last_spawn = 0
+                enemies_spawned += 1
+
+        if len(enemies_group) == 0 and enemies_spawned >= enemies_to_spawn:
+            spawn_timer += clock.get_time()
+            if spawn_timer >= spawn_interval:
+                start_new_wave()
+                spawn_timer = 0
 
     clock.tick(60)
-
-
-    if enemies_spawned < enemies_to_spawn:
-        time_since_last_spawn += clock.get_rawtime()
-        if time_since_last_spawn >= enemy_spawn_interval:
-            enemy_start_pos = (800, random.randint(15, 635))
-            enemy = Enemy(enemy_start_pos)
-            enemies_group.add(enemy)
-            time_since_last_spawn = 0
-            enemies_spawned += 1
-
-
-    if len(enemies_group) == 0 and enemies_spawned >= enemies_to_spawn:
-        spawn_timer += clock.get_rawtime()
-        if spawn_timer >= spawn_interval:
-            start_new_wave()
-            spawn_timer = 0
